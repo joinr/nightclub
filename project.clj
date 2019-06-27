6086ddfd27ab6330e0a00dc5f208ee13e7df4f9e
tom <tom@that-place>
2019-06-26 23:44:04 -0500

    bumped nc and clj deps

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
                 theoretically more robust.  At a minimum, re-use 
                 his widgets in a modular capacity.  Decouple the 
                 lein dependency and other build components..."
-  :dependencies [[joinr/nightcode "1.3.3-SNAPSHOT"]
-                 [org.clojure/clojure #_"1.8.0" "1.10.0"]
+  :dependencies [[joinr/nightcode "1.3.4-SNAPSHOT"]
+                 [org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.490"]
                  [seesaw "1.4.5"]]
   :source-paths  ["src" "../NightCode/src/clojure/"]   
