a1fce5e91b9cb721bb37170084c2ed8fe8d6c963
tom <tom@that-place>
2021-09-22 14:55:30 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject joinr/nightclub "0.0.3-SNAPSHOT"
+(defproject joinr/nightclub "0.0.4-SNAPSHOT"
   :description "This is a revisitation of the nightcode swing database.
                 Based on Zach Oakes's body of work.  The intent is to 
                 extend his working example from NightCode, and provide
@@ -7,10 +7,10 @@
                 theoretically more robust.  At a minimum, re-use 
                 his widgets in a modular capacity.  Decouple the 
                 lein dependency and other build components..."
-  :dependencies [[joinr/nightcode "1.3.6-SNAPSHOT"]
-                 [org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.4.490"]
-                 [seesaw "1.4.5"]]
+  :dependencies [[joinr/nightcode "1.3.7-SNAPSHOT"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.3.622"]
+                 [seesaw "1.5.0"]]
   :source-paths  ["src" "../Nightcode/src/clojure/"] 
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :profiles {:ordered {:aot [nightclub.core]} 
